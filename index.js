7e8356d2539ee10f833efc1842fdebe6857b1f04
kkolade <25391226+kkolade@users.noreply.github.com>
2022-11-22 16:19:23 +0100

    Update index.js
    Added event listener to all selected elements

--- index.js
+++ index.js
@@ -1,3 +1,23 @@
 const menuBtn = document.querySelector('.menu-btn');
 const headerNavMobile = document.querySelector('.header-nav-mobile');
 const cancelCross = document.querySelector('.cancel-cross');
+
+menuBtn.addEventListener('click', () => {
+    headerNavMobile.classList.remove('hidden')
+});
+
+cancelCross.addEventListener('click', () => {
+    headerNavMobile.classList.add('hidden')
+});
+
+linkSection[0].addEventListener('click', () => {
+    headerNavMobile.classList.toggle('hidden')
+});
+
+linkSection[1].addEventListener('click', () => {
+    headerNavMobile.classList.add('hidden')
+});
+
+linkSection[2].addEventListener('click', () => {
+    headerNavMobile.classList.add('hidden')
+});
