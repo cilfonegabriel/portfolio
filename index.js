3a98a70fb9e87193015ced18478b93906a8e8673
Gabriel Cilfone <79432268+cilfonegabriel@users.noreply.github.com>
2022-11-22 11:50:14 -0300

    changes js

--- index.js
+++ index.js
@@ -1,11 +1,1 @@
-const menuBtn = document.querySelector('.menu-btn');
-const headerNavMobile = document.querySelector('.header-nav-mobile');
-const cancelCross = document.querySelector('.cancel-cross');
 
-menuBtn.addEventListener('click', () => {
-    headerNavMobile.classList.remove('hidden')
-
-});
-cancelCross.addEventListener('click', () => {
-    headerNavMobile.classList.add('hidden')
-})
