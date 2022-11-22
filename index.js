2882d0bd13a25f958da0a1e672dd6d16e6742149
Pierre Barba <99061965+PierreBarba@users.noreply.github.com>
2022-11-22 08:51:40 -0600

    creat constant  to use with js

--- index.js
+++ index.js
@@ -1,1 +1,3 @@
-
+const menuBtn = document.querySelector('.menu-btn');
+const headerNavMobile = document.querySelector('.header-nav-mobile');
+const cancelCross = document.querySelector('.cancel-cross');
