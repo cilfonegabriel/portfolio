b78f7454e967513bbe10dde2184035631d4bf6e9
gabicilfone <cilfonegabriel@gmail.com>
2022-11-25 10:35:50 -0300

    changes in file js and css

--- index.js
+++ index.js
@@ -1,23 +1,32 @@
 const menuBtn = document.querySelector('.menu-btn');
 const headerNavMobile = document.querySelector('.header-nav-mobile');
 const cancelCross = document.querySelector('.cancel-cross');
+const linkPortfolio = document.querySelector('.menu-link');
+const linkAbout = document.querySelector('.menu-link');
+const linkContact = document.querySelector('.menu-link');
 
 menuBtn.addEventListener('click', () => {
+    menuBtn.classList.add('hidden')
     headerNavMobile.classList.remove('hidden')
 });
 
 cancelCross.addEventListener('click', () => {
+    menuBtn.classList.remove('hidden')
     headerNavMobile.classList.add('hidden')
 });
 
-linkSection[0].addEventListener('click', () => {
-    headerNavMobile.classList.toggle('hidden')
-});
-
-linkSection[1].addEventListener('click', () => {
-    headerNavMobile.classList.add('hidden')
-});
-
-linkSection[2].addEventListener('click', () => {
-    headerNavMobile.classList.add('hidden')
-});
+linkPortfolio.addEventListener('click', () => {
+    headerNavMobile.classList.add('hidden');
+    menuBtn.classList.remove('hidden');
+  });
+  
+  linkAbout.addEventListener('click', () => {
+    headerNavMobile.classList.add('hidden');
+    menuBtn.classList.remove('hidden');
+  });
+  
+  linkContact.addEventListener('click', () => {
+    headerNavMobile.classList.add('hidden');
+    menuBtn.classList.remove('hidden');
+  });
+  
