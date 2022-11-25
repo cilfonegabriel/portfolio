62633f1b6937d47fdddece4b41426c1e9da8359f
gabicilfone <cilfonegabriel@gmail.com>
2022-11-25 12:44:31 -0300

    changes js

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
 const menuBtn = document.querySelector('.menu-btn');
 const headerNavMobile = document.querySelector('.header-nav-mobile');
 const cancelCross = document.querySelector('.cancel-cross');
-const linkPortfolio = document.querySelector('.menu-link');
-const linkAbout = document.querySelector('.menu-link');
-const linkContact = document.querySelector('.menu-link');
+const linkPortfolio = document.querySelectorAll('.menu-link');
+//const linkAbout = document.querySelector('.menu-link');
+//const linkContact = document.querySelector('.menu-link');
 
 menuBtn.addEventListener('click', () => {
     menuBtn.classList.add('hidden')
@@ -20,13 +20,5 @@ linkPortfolio.addEventListener('click', () => {
     menuBtn.classList.remove('hidden');
   });
   
-  linkAbout.addEventListener('click', () => {
-    headerNavMobile.classList.add('hidden');
-    menuBtn.classList.remove('hidden');
-  });
-  
-  linkContact.addEventListener('click', () => {
-    headerNavMobile.classList.add('hidden');
-    menuBtn.classList.remove('hidden');
-  });
+
   
